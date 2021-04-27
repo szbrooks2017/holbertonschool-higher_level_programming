d8f3afc559a69af6c320c30e3090d6db841ce4a9
szbrooks2017 <strattonbrooks13@gmail.com>
2021-04-27 22:45:01 +0000

    adding DIFFICULT comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        if (i != j and i < 9):
+        if (i != j and i < 9 and i < j):
             print('{0}{1}, '.format(i, j), end='')
