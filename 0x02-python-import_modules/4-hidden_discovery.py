c6f3c54441e801f46a6db683e6df8cc49dbe960d
szbrooks2017 <strattonbrooks13@gmail.com>
2021-04-28 04:57:35 +0000

    pep8

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    for i in dir(hiden_4):
+        if i.startswith("__"):
+            continue
+        else:
+            print("{}".format(i))
