3522ed42455a129320a5d034e601e35fb1ba9649
szbrooks2017 <strattonbrooks13@gmail.com>
2021-04-28 01:38:56 +0000

    printing alph

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for lower in reversed(range(97, 123)):
-    print("{}".format(chr(lower)), end="")
+    if lower % 2 == 0:
+	print("{}".format(chr(lower)), end="")
+    else:
+        print("{}".format(chr(lower - 32)), end="")
