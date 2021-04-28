0893981da5bb9e84e09c49b32eb9decbc8ea1173
szbrooks2017 <strattonbrooks13@gmail.com>
2021-04-28 20:59:20 +0000

    how to print a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[}}):
+def print_matrix_integer(matrix=[[]]):
     if matrix:
         for row in range(len(matrix)):
-            for column in range(len(row)):
+            for column in range(len(matrix[row])):
                 print(matrix[row][column], end=' ')
             print()
