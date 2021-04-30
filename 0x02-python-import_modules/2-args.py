f8eea4341d00401a1df8be5fed9564594a23cda9
szbrooks2017 <strattonbrooks13@gmail.com>
2021-04-30 17:56:41 +0000

    fixing pep8

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ import sys
 if __name__ == '__main__':
     n = len(sys.argv)
     if n == 1:
-        print("{}".format("0 arguments."))
+        print("0 arguments.")
     elif n == 2:
-        print("{}".format("1 argument:"))
+        print("1 argument:")
     else:
-        print("{:d} {}".format(n - 1, "arguments:", end=""))
-        for i in range(1, n):
-            print("{:d}: {}".format(i, sys.argv[i]))
+        print("{} arguments:".format(n - 1))
+    for i in range(1, n):
+        print("{}: {}".format(i, sys.argv[i]))
