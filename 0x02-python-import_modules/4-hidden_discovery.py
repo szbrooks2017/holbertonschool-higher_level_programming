7c64a56640d89634bae09d3a6fdba30cba9b8833
szbrooks2017 <strattonbrooks13@gmail.com>
2021-04-30 17:57:50 +0000

    fixing pep8

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-if __name__== "__main__":
-    for i in dir(hiden_4):
+if __name__ == "__main__":
+    for i in dir(hidden_4):
         if i.startswith("__"):
             continue
         else:
