1cb2247320a2dbf215daf1329e6b0e92c7af1998
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-04 05:14:46 +0000

    How to divide a list safely

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-"""
-#mylist1 and 2 can contain any type
-list length can be bigger than both lists
-return  a new list with all divisions
-if 2 elements cant be divided results = 0
-if an element is not an int/float print wrong type
-if division cant be done print division by 0
-if mylist 1 or 2 is too short print out of range
-"""
+
 
 def list_division(my_list_1, my_list_2, list_length):
 
