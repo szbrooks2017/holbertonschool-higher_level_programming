089f6d8a95714dbac27a8035d8c208fa4f24359c
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-04 05:05:13 +0000

    How to print ints safely

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@
 def safe_print_list(my_list=[], x=0):
 
     count = 0
-    for count in range(x):
+    for count in range(0, x):
         try:
             print(my_list[i], end="")
             count += 1
