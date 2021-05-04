ef5d53285116bf1e058de3ce89a9d393b7bcf1ae
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-04 05:25:15 +0000

    How to print a list safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
             count += 1
-        except IndexError:
+        except:
             break
 
     print()
