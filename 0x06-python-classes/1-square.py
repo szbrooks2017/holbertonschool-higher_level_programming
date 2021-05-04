e8f9a6d8267fd7544d2e7fc3cdc3d063f6bba06c
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-04 23:50:47 +0000

    How to initialize an attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" a Square with size, private instance attriute size, no type/value"""
+
+
+class Square:
+
+    """a class that creates a single attribute"""
+
+    def __init__(self, size):
+        self.__size = size
