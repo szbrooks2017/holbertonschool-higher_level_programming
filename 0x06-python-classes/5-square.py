0dc078b419e91dc37571b8036e6ae445fd1e707a
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-05 01:36:49 +0000

    How to print an area of a class

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+""" a Square with size, private instance attribute size
+    prperty def size(self) to retrieve size
+    prperty def size(self, value)
+    size is an int else raise typeerror, exception
+    if size < 0 raise valueerror, exception
+    public instance def area
+    public instnace print to stdout with #
+    if size = 0 print empty line
+"""
+
+
+class Square:
+
+    """a class that returns the area of a square """
+
+    def __init__(self, size=0):
+
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        square_area = self.__size * self.__size
+        return square_area
+
+    def my_print(self):
+        if self.__size is 0:
+            pass
+        for j in range(self.__size):
+            for i in range(self.__size):
+                print("#", end="")
+        print()
