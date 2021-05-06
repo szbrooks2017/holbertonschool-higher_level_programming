aa679a7fe0741d3a839d04e56d2a2040330b6e53
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-06 00:54:40 +0000

    How to find area and perimeter of a rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+a class rectangle
+##private instance, property setter, -width, must be an int or typeerror
+a width < 0 or valueerror
+##private instance - height, property setter, -height must be int or typeerror
+height < 0 valueerror
+##public instance - area that returns area, perimeter that returns perimeter
+if width/height == = perimeter = 0
+"""
+
+
+class Rectangle:
+    """two private instances"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        square_area = self.__height * self.__width
+        return square_area
+
+    def perimeter(self):
+        p = 2*(self.__height + self.__width)
+        return p
