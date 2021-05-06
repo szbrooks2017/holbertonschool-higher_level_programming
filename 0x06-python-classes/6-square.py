a4c128703958f3a75630926fda3393281204d2fb
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-06 19:29:41 +0000

    How to do multiple instances and methods

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -25,8 +25,6 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(size) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if size < 0:
-            raise ValueError("size must be >= 0")
 
     @property
     def size(self):
