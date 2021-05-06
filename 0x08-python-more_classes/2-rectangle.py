2ec4b999aa784a36eb0b6e16d5bbe3a4a0f7be01
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-06 01:04:35 +0000

    How to find area and perimeter of a rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -48,5 +48,7 @@ class Rectangle:
         return square_area
 
     def perimeter(self):
+        if self.height == 0 or self.width == 0:
+            return 0
         p = 2*(self.__height + self.__width)
         return p
