b2d745c517da52dc8d7c03bdafa960397567a8b9
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-10 03:57:21 +0000

    How to use unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,11 +9,11 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """ test for max int """
 
-    def test_list(self):
+    def test_max(self):
         self.assertEqual(max_integer([1, 2, 3]), 3)
         self.assertEqual(max_integer([-1, -2, -3]), -1)
-        selfassertIsNone(max_integer([]))
-        selfassertIsNone(max_integer([1]), 1)
+        self.assertIsNone(max_integer([]))
+        self.assertIsNone(max_integer([1]), 1)
 
 if __name__ =='__main__':
     unittest.main()
