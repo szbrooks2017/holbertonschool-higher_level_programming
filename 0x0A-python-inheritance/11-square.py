933fdcba513ae597f0e9d95175c2e2ebd9a78f8f
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-11 23:14:23 +0000

    How to create a sibling class of an inherited class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" inherit an inherited class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ inception """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__height = size
+        self.__width = size
+
+    def __str__(self):
+     return "[Square] {}/{}".format(self.__width, self.__height)
