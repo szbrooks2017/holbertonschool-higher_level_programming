18e91f820d6f926b7a4c5c9a6a69e0b8d0e526e6
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-11 18:48:40 +0000

    How to inherit list and a method to print it sorted

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyton3
+""" a class that inehrits from list"""
+
+class MyList(list):
+    """print the list but sorted"""
+    def __init__(self):
+        super().__init__()
+
+    def print_sorted(self):
+        print(sorted(self))
