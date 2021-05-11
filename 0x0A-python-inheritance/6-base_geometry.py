ebfaa2d99373a579aff0fcce99d26e360dde2378
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-11 20:45:23 +0000

    How to define an area in a class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Class BaseGeometry, def area, exception"""
+
+
+class BaseGeometry:
+    """ exception in area"""
+    def area(self):
+        raise Exception("area() is not implemented")
