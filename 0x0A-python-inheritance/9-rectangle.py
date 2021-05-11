450a98448b9581a8a8952ea726189c4ae2468363
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-11 22:53:13 +0000

    How to add an area in an inherited method

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" a rectangle class that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ creates a rectangle"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
