ea1b73a494a6e7df6f5576a2577202971dc39b31
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-11 20:05:40 +0000

    How to use isinstance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" return true of an instance of the inherited class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ returns true if an instance """
+    return isinstance(obj, a_class)
