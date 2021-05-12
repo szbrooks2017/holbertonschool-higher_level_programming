0dd6a0cede194c41e09877eeae88020cb3944cb8
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-12 23:39:55 +0000

    How to use write()

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,7 @@
 
 def write_file(filename="", text=""):
     """ write a string to UTF8, return characters"""
-    with open(filename, 'r', encoding='utf8') as f:
-        charCount = len(text)
-        return charCount
+    with open(filename, 'w', encoding='utf8') as jfile:
+        jfile.write(text)
+        count = len(text)
+        return count
