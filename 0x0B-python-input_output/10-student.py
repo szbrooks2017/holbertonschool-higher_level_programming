2a0bbd3b7c2432fcd8de59d8bbfa2cfdf1d43d99
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-13 04:22:24 +0000

    How to single out attributes for json

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" a class Student"""
+
+
+class Student:
+    """ first_name, last_name, age"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        new_dict = {}
+        if attrs is None:
+            return vars(self)
+
+        for key in attrs:
+            if key in self.__dict__:
+                new_dict[key] = self.__dict__[key]
+                return new_dict
