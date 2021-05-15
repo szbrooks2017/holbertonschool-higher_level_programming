8a586d15aab40db1792fa088eaecef8c154f7c52
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-15 17:52:49 +0000

    Creating the base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,13 +4,18 @@
     this class is the base of all other classes in the project.
     the goal is to manage id attribute in all future classes to
     avoid duplicating the same code
-""""
+"""
+
 
 class Base:
+
     """ create constructor if id is not none, assign id with this value """
     __nb_objects = 0
+    newValue = 0
+
     def __init__(self, id=None):
-        if id is not None
+        if id is not None:
             self.id = id
-        __nb_objects += newValue
-        self.newValue = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
