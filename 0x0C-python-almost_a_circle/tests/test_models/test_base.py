f9769dc572644cbee8599ab5c220baece2f9934c
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-15 18:13:08 +0000

    Adding unittests for base

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" tests for the base class"""
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """tests for base"""
+
+    def test_base(self):
+        self.b1 = Base()
+
