f97f0124ed1745eded6b53f4ed3e6dc2b48620c1
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-15 18:35:48 +0000

    Adding unittests for base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,5 +8,12 @@ class TestBase(unittest.TestCase):
 
     """tests for base"""
 
-    def test_base(self):
+    def setUp(self)
+        pass
+
+    def test_base_for_ID(self):
         self.b1 = Base()
+        self.assertTrue(b1.id, 1)
+
+if __name__ == '__main__':
+    unittest.main()
