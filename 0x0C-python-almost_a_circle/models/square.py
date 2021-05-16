7571d8c79ab569408d390f87159121dc464ca3b2
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-16 22:36:06 +0000

    Adding Square, inheriting from rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,9 +9,11 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     def __str__(self):
         """ overiding str with new specifications"""
-        return "[Square] ({})\
-                 {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        i = self.id
+        s = self.width
+        x = self.x
+        y = self.y
+        return "[Square] ({}) {}/{} - {}".format(i, x, y, s)
