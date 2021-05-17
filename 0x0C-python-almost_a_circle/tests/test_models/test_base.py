b85166f304ec5fb2c887fec73c698adc6eb44172
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-17 22:20:31 +0000

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """ tests for the base class"""
+
 import unittest
 from models.base import Base
-
 
 class TestBase(unittest.TestCase):
 
     """tests for base"""
-
-    def setUp(self):
-        pass
 
     def test_base_for_ID(self):
         b1 = Base()
@@ -20,6 +17,13 @@ class TestBase(unittest.TestCase):
     def test_if_id_match(self):
         b98 = Base(98)
         self.assertEqual(b98.id, 98)
+
+    def test_from_json(self):
+        obj = Base.from_json_string(None)
+        self.assertEqual(obj, [])
+
+        obj = Base.from_json_string("[]")
+        self.assertEqual(obj, [])
 
 if __name__ == '__main__':
     unittest.main()
