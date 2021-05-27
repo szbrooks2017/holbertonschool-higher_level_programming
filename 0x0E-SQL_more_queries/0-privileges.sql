4265057b8279290520e1010e0b1011311706582e
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-27 20:12:45 +0000

    How to list all privileges of a user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- list all privileges of MySQL users.
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- second comment.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
