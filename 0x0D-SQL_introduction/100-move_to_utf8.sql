554809c61aa3a16c354e1bad44aa84399297ef0a
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-27 02:15:53 +0000

    adding advanced task

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- convert to utf8.
+ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE htbn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
