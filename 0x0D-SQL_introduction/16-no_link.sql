4296752ccf36c6bdca8fe430efbe6f0dfbb0b351
szbrooks2017 <strattonbrooks13@gmail.com>
2021-05-27 01:59:00 +0000

    How to arrange by data with exception

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,4 @@
 -- list all records of a table.
-SELECT DISTINCT score FROM second_table WHERE name is not null ORDER BY score DESC;	
+SELECT score, name FROM second_table 
+WHERE name is not null 
+ORDER BY score DESC;	
