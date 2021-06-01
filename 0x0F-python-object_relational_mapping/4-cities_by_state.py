c1f75b30ca8c1005e22026f564d924760dbebf3f
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-01 23:07:53 +0000

    select cities with Inner Join

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""list all states from database hbtn_0e_0_usa"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    with db.cursor() as cur:
+        cur.execute("SELECT cities.id, cities.name, states.name\
+                    FROM cities INNER JOIN states ON cities.state_id=states.id")
+        states = cur.fetchall()
+        for state in states:
+            print(state)
+    cur.close()
+    db.close()
