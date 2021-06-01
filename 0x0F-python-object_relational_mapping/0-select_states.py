923938f382c11e64b719a755a9214d5358944e9e
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-01 15:37:11 +0000

    How to conenct to a mysqldb with python

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
-    db = MySQLdb.connect(argv[1], argv[2], argv[3])
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * from states ORDER BY states.id")
