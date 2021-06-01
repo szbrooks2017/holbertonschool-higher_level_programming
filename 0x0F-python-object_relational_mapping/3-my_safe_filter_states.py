6a905e725fcc0692adcc6e4ba4e2a5e5d65b6dd7
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-01 22:34:29 +0000

    How to take arguments and displays values safe of injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""list all states from database hbtn_0e_0_usa"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+
+    with db.cursor() as cur:
+        cur.execute("SELECT * FROM states WHERE\
+                    name LIKE BINARY %(name)s ORDER\
+                    BY states.id ASC", {'name': argv[4]})
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
