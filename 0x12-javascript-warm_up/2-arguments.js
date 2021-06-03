c703d2c356bf2937279c491b2cc7d892708d96a5
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-03 01:41:19 +0000

    How to interpret command arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const myVar1 = 'No argument'
+const myVar2 = 'Argument found'
+const myVar3 = 'Arguments found'
+
+if (process.argv.length > 3) {
+	console.log(myVar3);
+} else if (process.argv.length === 3) {
+	console.log(myVar2);
+} else {
+	console.log(myVar1);
+}
