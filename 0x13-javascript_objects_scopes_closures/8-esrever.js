f2581b7af2ef630b28f8fb80e6cf029286fa8fe9
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-04 04:44:10 +0000

    How to reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const reversed = list.reverse();
+  const reversed = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversed.push(list[i]);
+  }
   return reversed;
 };
